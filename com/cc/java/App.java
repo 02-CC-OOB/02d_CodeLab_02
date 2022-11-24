e11ea228ced3ddfc643d3f92650f442f7e4c7d0b
sebas <cell333666@gmail.com>
2022-11-24 17:33:40 +0100

    l1

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -3,8 +3,22 @@ package com.cc.java;
 public class App {
     
     public static void main(String[] args) {
-     
+
+        Mitarbeiter mitarbeiter = new Mitarbeiter("otto", "peter", "worker", 1990);
+
+        output(mitarbeiter.getInfo("familyName") + " " + mitarbeiter.getInfo("firstName"));
+
+
+
+
+
+
     }
+
+
+
+
+
 
     public static void output(String outputStr) {
         System.out.println(outputStr);
